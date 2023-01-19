f16358f927342edf06220d546c2e413c99083fdd
Indranil Halder <indranilhalder.dev@gmail.com>
2023-01-20 02:51:08 +0530

    Create Read Update and Delete Events

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -7,6 +7,63 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+  // Create event with link user
+  async create(ctx) {
+    const { id } = ctx.state.user; //ctx.state.user contains the current authenticated user
+    const response = await super.create(ctx);
+    const updatedResponse = await strapi.entityService.update(
+      "api::event.event",
+      response.data.id,
+      { data: { user: id } }
+    );
+    return updatedResponse;
+  },
+
+  // Update user event
+  async update(ctx) {
+    // ?console.log(ctx.request.body);
+    // ?console.log(ctx.request.files);
+    const [event] = await strapi.entityService.findMany("api::event.event", {
+      filters: {
+        id: ctx.request.params.id,
+        user: ctx.state.user.id,
+      },
+    });
+
+    if (event) {
+      const response = await super.update(ctx);
+      return response;
+    } else {
+      return ctx.unauthorized();
+    }
+  },
+
+  // Delete User event
+  async delete(ctx) {
+    const [event] = await strapi.entityService.findMany("api::event.event", {
+      filters: {
+        id: ctx.request.params.id,
+        user: ctx.state.user.id,
+      },
+    });
+    if (event) {
+      const response = await super.delete(ctx);
+      return response;
+    } else {
+      return ctx.unauthorized();
+    }
+  },
+
+  //Find with populate
+  async find(ctx) {
+    const populateList = ["image", "user"];
+    // Push any additional query params to the array
+    populateList.push(ctx.query.populate);
+    ctx.query.populate = populateList.join(",");
+    const content = await super.find(ctx);
+    return content;
+  },
+
   // Get Logged in users
   async me(ctx) {
     const user = ctx.state.user;
